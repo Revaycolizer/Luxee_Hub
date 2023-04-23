c85e4e10c17ae5fae82f1b8514835e49df6b299b
Revaycolizer <109684996+Revaycolizer@users.noreply.github.com>
2023-04-23 16:44:54 +0300

    Update loginbtn.tsx

--- components/loginbtn.tsx
+++ components/loginbtn.tsx
@@ -13,12 +13,12 @@ const buttonVariants = cva(
         destructive:
           "bg-red-500 text-white hover:bg-red-600 dark:hover:bg-red-600",
         outline:
-          "bg-transparent border border-slate-200 hover:bg-slate-100 dark:border-slate-700 dark:text-slate-100",
+          "bg-slate-900 border border-slate-200 hover:bg-slate-100 dark:border-slate-700 dark:text-slate-100",
         subtle:
           "bg-slate-100 text-slate-900 hover:bg-slate-200 dark:bg-slate-700 dark:text-slate-100",
         ghost:
-          "bg-transparent hover:bg-slate-100 dark:hover:bg-slate-800 dark:text-slate-100 dark:hover:text-slate-100 data-[state=open]:bg-transparent dark:data-[state=open]:bg-transparent",
-        link: "bg-transparent dark:bg-transparent underline-offset-4 hover:underline text-slate-900 dark:text-slate-100 hover:bg-transparent dark:hover:bg-transparent",
+          "bg-slate-900 hover:bg-slate-100 dark:hover:bg-slate-800 dark:text-slate-100 dark:hover:text-slate-100 data-[state=open]:bg-slate-900 dark:data-[state=open]:bg-slate-900",
+        link: "bg-slate-900 dark:bg-slate-600 underline-offset-4 hover:underline text-slate-900 dark:text-slate-100 hover:bg-slate-900 dark:hover:bg-slate-900",
       },
       size: {
         default: "h-10 py-6 px-4",
@@ -51,3 +51,4 @@ const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
 Button.displayName = "Button"
 
 export { Button, buttonVariants }
+
