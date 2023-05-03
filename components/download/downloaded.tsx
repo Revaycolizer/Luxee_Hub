6e688765ed41743292782f67bb3c077e028e06e7
Revaycolizer <109684996+Revaycolizer@users.noreply.github.com>
2023-05-03 09:34:04 +0300

    Update downloaded.tsx

--- components/download/downloaded.tsx
+++ components/download/downloaded.tsx
@@ -1,15 +1,15 @@
-interface Props{
-data:Blob;
-}
+// interface Props{
+// data:Blob;
+// }
 
 
 
-import React from 'react'
+// import React from 'react'
 
-const Downloads = ({download}:{download:Props}) => {
-  return (
-    <div>{download.data}</div>
-  )
-}
+// const Downloads = ({download}:{download:Props}) => {
+//   return (
+//     <div>{download.data}</div>
+//   )
+// }
 
-export default Downloads
+// export default Downloads
